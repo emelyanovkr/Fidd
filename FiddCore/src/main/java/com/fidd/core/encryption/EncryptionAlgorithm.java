14442789640c7a4771e680ffc99882d112882e41
codekrolik2 <john.amirov.83@gmail.com>
2026-05-27 15:12:30 -0800

    TODO related to plaintext size added

--- FiddCore/src/main/java/com/fidd/core/encryption/EncryptionAlgorithm.java
+++ FiddCore/src/main/java/com/fidd/core/encryption/EncryptionAlgorithm.java
@@ -8,8 +8,13 @@ import java.io.OutputStream;
 import java.util.List;
 
 public interface EncryptionAlgorithm extends NamedEntry {
-    // TODO: this may be impossible to calculate precisely for some algorithms, e.g. AES/CBC/PKCS5Padding
     default long plaintextLengthToCiphertextLength(long plaintextLength) { return plaintextLength; }
+
+    // TODO: ciphertextLengthToPlaintextLength defaults to returning the ciphertext size, but registered padded
+    //  algorithms such as AES-256-CBC produce ciphertext that includes PKCS#5 padding.
+    //  For those files this sets Content-Length/range bounds larger than the decrypted stream, which can break
+    //  full downloads. Store the plaintext length in the Fidd key/metadata or otherwise avoid advertising a decoded
+    //  length that cannot be derived from the ciphertext length.
     default long ciphertextLengthToPlaintextLength(long ciphertextLength) { return ciphertextLength; }
 
     String UNENCRYPTED = "UNENCRYPTED";
